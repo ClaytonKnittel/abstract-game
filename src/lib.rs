e13555ca03812101d4c2472074d950a584bb078f
Clayton Knittel <cknit1999@gmail.com>
2025-09-07 14:22:14 -0700

    abstract game

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod game;
+mod packed_score;
+mod score;
+mod simple_solver;
+mod solver;
+pub mod test_games;
+mod util;
+
+pub use game::*;
+pub use packed_score::*;
+pub use score::*;
+pub use simple_solver::*;
+pub use solver::*;
